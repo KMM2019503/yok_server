94634c243744aa798270fb25aa326b51e9e29bc5
Isaac4772 <aungpying942@gmail.com>
2024-10-23 20:11:24 +0700

    refactor(firebase-auth): refactor checkToken middleware to use Firebase admin for token verification

--- src/v1/middlewares/checkAuth.js
+++ src/v1/middlewares/checkAuth.js
@@ -1,22 +1,22 @@
-import jwt from "jsonwebtoken";
+import admin from "../utils/firebase"; // Import your Firebase admin instance
 import logger from "../utils/logger";
 
-export const checkToken = (req, res, next) => {
-  const AuthToken = req.cookies.AuthToken;
+export const checkToken = async (req, res, next) => {
+  const AuthToken = req.cookies.AuthToken; // Assuming the token is stored in cookies
   if (!AuthToken) {
     return res.status(401).json({
       success: false,
-      message: "Unauthorized - You have not permission to access this route",
+      message: "Unauthorized - You do not have permission to access this route",
     });
   }
 
   try {
-    const decoded = jwt.verify(AuthToken, process.env.JWT_SECRET_KEY);
-
-    req.userId = decoded.userId;
-    next();
+    // Verify the Firebase ID token
+    const decoded = await admin.auth().verifyIdToken(AuthToken);
+    req.userId = decoded.uid; // Attach user ID to the request object
+    next(); // Proceed to the next middleware or route handler
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
+    if (error.code === "auth/id-token-expired") {
       return res.status(401).json({
         success: false,
         message: "Token expired. Please log in again.",
